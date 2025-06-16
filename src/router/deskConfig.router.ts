60e7d086eab8f1dd3a8a59e7ddc61f977e0aed7f
shuisheng <2274751790@qq.com>
2025-06-16 13:30:51 +0800

    docs: 更新文档

--- src/router/deskConfig.router.ts
+++
@@ -1,9 +0,0 @@
-import Router from 'koa-router';
-
-import deskConfigController from '@/controller/deskConfig.controller';
-
-const deskConfigRouter = new Router({ prefix: '/desk_config' });
-
-deskConfigRouter.get('/turnserver', deskConfigController.turnserver);
-
-export default deskConfigRouter;
--- src/router/shorturl.router.ts
+++
@@ -1,9 +0,0 @@
-import Router from 'koa-router';
-
-import shorturlController from '@/controller/shorturl.controller';
-
-const shorturlRouter = new Router({ prefix: '/shorturl' });
-
-shorturlRouter.get('/find/:code', shorturlController.find);
-
-export default shorturlRouter;
